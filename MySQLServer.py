261a65e043483eb239f163086cc3fed2b750efdc
sara amti <saraamti0@gmail.com>
2024-10-20 18:01:04 +0100

    my server

--- MySQLServer.py
+++ MySQLServer.py
@@ -12,12 +12,12 @@ def create_database():
 
         if connection.is_connected():
             cursor = connection.cursor()
-            # Create database if it doesn't exist
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error: {e}")
+    
     finally:
         if connection.is_connected():
             cursor.close()
@@ -26,4 +26,5 @@ def create_database():
 
 if __name__ == "__main__":
     create_database()
+
 
